51083fb0236858f8cccc46809d1c3a0fa7d059a3
Alexander Krainev <a.krainev02@gmail.com>
2021-10-16 11:22:51 +0300

    new version

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -33,7 +33,7 @@ TEST(TMatrix, copied_matrix_has_its_own_memory){
 
 TEST(TMatrix, can_get_size){
 	TMatrix<int> m(10);
-	ASSERT_NO_THROW(m.GetSize());
+	EXPECT_EQ(10,m.GetSize());
 }
 
 TEST(TMatrix, can_set_and_get_element){
